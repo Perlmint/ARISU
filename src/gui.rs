a4a5b439e96ac3fa68f780ce2a3f795b2dd63a6b
Gyusun Yeom <omniavinco@gmail.com>
2025-06-01 20:33:30 +0900

    Add status bar item

--- src/gui.rs
+++ src/gui.rs
@@ -1,18 +1,94 @@
-use crate::counter::Interval;
+#![deny(unsafe_op_in_unsafe_fn)]
+use std::cell::Cell;
 
-pub struct App {
+use crate::counter::Interval;
+use objc2::rc::Retained;
+use objc2::runtime::ProtocolObject;
+use objc2::{define_class, msg_send, DefinedClass, MainThreadMarker, MainThreadOnly};
+use objc2_app_kit::{
+    NSApplication, NSApplicationActivationPolicy, NSApplicationDelegate, NSImage, NSMenu,
+    NSStatusBar, NSVariableStatusItemLength,
+};
+use objc2_foundation::{NSNotification, NSObject, NSObjectProtocol, NSString};
+
+struct Ivars {
     capture_interval: Interval,
     display_send_interval: Interval,
+    status_bar: Cell<Option<Retained<NSStatusBar>>>,
 }
 
-impl App {
-    pub fn new(capture_interval: Interval, display_send_interval: Interval) -> Self {
-        tui_logger::init_logger(tui_logger::LevelFilter::Trace).unwrap();
-        tui_logger::set_default_level(tui_logger::LevelFilter::Info);
+define_class!(
+    // SAFETY:
+    // - The superclass NSObject does not have any subclassing requirements.
+    // - `AppDelegate` does not implement `Drop`.
+    #[unsafe(super(NSObject))]
+    #[thread_kind = MainThreadOnly]
+    #[ivars = Ivars]
+    struct AppDelegate;
 
-        Self {
-            capture_interval,
-            display_send_interval,
+    unsafe impl NSObjectProtocol for AppDelegate {}
+
+    unsafe impl NSApplicationDelegate for AppDelegate {
+        #[unsafe(method(applicationDidFinishLaunching:))]
+        fn did_finish_launching(&self, _notification: &NSNotification) {
+            let mtm = MainThreadMarker::from(self);
+
+            self.init_status_item(mtm);
+
+            NSApplication::main(mtm);
+        }
+
+        #[unsafe(method(applicationWillTerminate:))]
+        fn will_terminate(&self, _notification: &NSNotification) {
+            println!("Will terminate!");
         }
     }
+);
+
+impl AppDelegate {
+    fn new(
+        capture_interval: Interval,
+        display_send_interval: Interval,
+        mtm: MainThreadMarker,
+    ) -> Retained<Self> {
+        let this = Self::alloc(mtm);
+        let this = this.set_ivars(Ivars {
+            capture_interval,
+            display_send_interval,
+            status_bar: Cell::new(None),
+        });
+        unsafe { msg_send![super(this), init] }
+    }
+
+    fn init_status_item(&self, mtm: MainThreadMarker) {
+        let status_bar = unsafe { NSStatusBar::new() };
+        let status_bar_item =
+            unsafe { status_bar.statusItemWithLength(NSVariableStatusItemLength) };
+        if let Some(button) = unsafe { status_bar_item.button(mtm) } {
+            let image = unsafe {
+                NSImage::imageWithSystemSymbolName_accessibilityDescription(
+                    &NSString::from_str("apple.logo"),
+                    None,
+                )
+            };
+            unsafe { button.setImage(image.as_deref()) };
+        }
+
+        self.ivars().status_bar.set(Some(status_bar))
+    }
+}
+
+pub fn run(capture_interval: Interval, display_send_interval: Interval) {
+    let mtm: MainThreadMarker = MainThreadMarker::new().unwrap();
+
+    let app = NSApplication::sharedApplication(mtm);
+    app.setActivationPolicy(NSApplicationActivationPolicy::Accessory);
+
+    // configure the application delegate
+    let delegate = AppDelegate::new(capture_interval, display_send_interval, mtm);
+    let object = ProtocolObject::from_ref(&*delegate);
+    app.setDelegate(Some(object));
+
+    // run the app
+    app.run();
 }
--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ mod counter;
 
 // mod clipboard;
 // mod credential;
+mod gui;
 mod input;
 mod screen;
 
@@ -40,20 +41,14 @@ struct Args {
     security: Security,
 }
 
-#[cfg(feature = "gui")]
-mod gui;
-#[cfg(feature = "gui")]
-use gui::App;
-
-#[tokio::main]
-async fn main() -> Result<(), anyhow::Error> {
-    let top_local_set = tokio::task::LocalSet::new();
-    let mut join_set = tokio::task::JoinSet::new();
-
+fn main() -> Result<(), anyhow::Error> {
     let args = Args::parse();
 
     let capture_counter = IntervalCounter::new();
     let display_send_counter = IntervalCounter::new();
+
+    let capture_counter_interval = capture_counter.interval();
+    let display_send_counter_interval = display_send_counter.interval();
 
     use tracing_subscriber::{filter::LevelFilter, fmt, EnvFilter};
     fmt()
@@ -61,83 +56,94 @@ async fn main() -> Result<(), anyhow::Error> {
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
-    let _server_handle = join_set.spawn_local_on(
-        async move {
-            let local_set = tokio::task::LocalSet::new();
-            let security = args.security;
+    std::thread::spawn(move || {
+        let rt = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .expect("Failed to build tokio runtime");
+        rt.block_on(async move {
+            let top_local_set = tokio::task::LocalSet::new();
 
-            tracing::info!("Building RDP server");
-            let server_builder =
-                RdpServer::builder().with_addr((IpAddr::from_str(&args.host)?, args.port));
+            let mut join_set = tokio::task::JoinSet::new();
+            let _server_handle = join_set.spawn_local_on(
+                async move {
+                    let local_set = tokio::task::LocalSet::new();
+                    let security = args.security;
 
-            let server_builder = if let Some((cert_path, key_path)) = args.certificate.zip(args.key)
-            {
-                let identity = TlsIdentityCtx::init_from_paths(&cert_path, &key_path)
-                    .context("failed to init TLS identity")?;
-                let acceptor = identity
-                    .make_acceptor()
-                    .context("failed to build TLS acceptor")?;
+                    tracing::info!("Building RDP server");
+                    let server_builder =
+                        RdpServer::builder().with_addr((IpAddr::from_str(&args.host)?, args.port));
 
-                if security == Security::Hybrid {
-                    server_builder.with_hybrid(acceptor, identity.pub_key)
-                } else {
-                    server_builder.with_tls(acceptor)
+                    let server_builder =
+                        if let Some((cert_path, key_path)) = args.certificate.zip(args.key) {
+                            let identity = TlsIdentityCtx::init_from_paths(&cert_path, &key_path)
+                                .context("failed to init TLS identity")?;
+                            let acceptor = identity
+                                .make_acceptor()
+                                .context("failed to build TLS acceptor")?;
+
+                            if security == Security::Hybrid {
+                                server_builder.with_hybrid(acceptor, identity.pub_key)
+                            } else {
+                                server_builder.with_tls(acceptor)
+                            }
+                        } else if security == Security::None {
+                            server_builder.with_no_security()
+                        } else {
+                            anyhow::bail!("Security is specified. but cert is not specified");
+                        };
+
+                    tracing::info!("Create clipboard server");
+                    // let cliprdr = Box::new(StubCliprdrServerFactory::new());
+
+                    tracing::info!("Create display handler");
+                    let (screen_handler, screen_job_processor) =
+                        ScreenCapture::new(&local_set, capture_counter, display_send_counter)?;
+
+                    let mut server = server_builder
+                        .with_input_handler(screen_handler.input_handler())
+                        .with_display_handler(screen_handler.clone())
+                        // .with_cliprdr_factory(Some(cliprdr))
+                        // .with_sound_factory(Some(Box::new(screen_handler)))
+                        .build();
+
+                    server.set_credentials(Some(Credentials {
+                        username: "user".to_string(),
+                        password: "user".to_string(),
+                        domain: None,
+                    }));
+
+                    let server_join_handler = local_set.spawn_local(async move {
+                        tracing::info!("Starting server");
+                        if let Err(e) = server.run().await {
+                            tracing::error!(?e, "Server run error");
+                        }
+                    });
+
+                    local_set.await;
+                    server_join_handler.await.context("server error")?;
+                    screen_job_processor
+                        .await
+                        .context("display job join error")
+                        .and_then(|i| i.context("diaply job error"))?;
+
+                    Ok(())
+                },
+                &top_local_set,
+            );
+            join_set.spawn(async move { Result::<(), anyhow::Error>::Ok(()) });
+
+            tracing::info!("Start server");
+            let (_, join_ret) = tokio::join!(top_local_set, join_set.join_all(),);
+            for ret in join_ret {
+                if let Err(e) = ret {
+                    error!(?e);
                 }
-            } else if security == Security::None {
-                server_builder.with_no_security()
-            } else {
-                anyhow::bail!("Security is specified. but cert is not specified");
-            };
-
-            tracing::info!("Create clipboard server");
-            // let cliprdr = Box::new(StubCliprdrServerFactory::new());
-
-            tracing::info!("Create display handler");
-            let (screen_handler, screen_job_processor) =
-                ScreenCapture::new(&local_set, capture_counter, display_send_counter)?;
-
-            let mut server = server_builder
-                .with_input_handler(screen_handler.input_handler())
-                .with_display_handler(screen_handler.clone())
-                // .with_cliprdr_factory(Some(cliprdr))
-                // .with_sound_factory(Some(Box::new(screen_handler)))
-                .build();
-
-            server.set_credentials(Some(Credentials {
-                username: "user".to_string(),
-                password: "user".to_string(),
-                domain: None,
-            }));
-
-            let server_join_handler = local_set.spawn_local(async move {
-                tracing::info!("Starting server");
-                if let Err(e) = server.run().await {
-                    tracing::error!(?e, "Server run error");
-                }
-            });
-
-            local_set.await;
-            server_join_handler.await.context("server error")?;
-            screen_job_processor
-                .await
-                .context("display job join error")
-                .and_then(|i| i.context("diaply job error"))?;
-
-            Ok(())
-        },
-        &top_local_set,
-    );
-    join_set.spawn(async move {
-        Result::<(), anyhow::Error>::Ok(())
+            }
+        });
     });
 
-    tracing::info!("Start server");
-    let (_, join_ret) = tokio::join!(top_local_set, join_set.join_all(),);
-    for ret in join_ret {
-        if let Err(e) = ret {
-            error!(?e);
-        }
-    }
+    gui::run(capture_counter_interval, display_send_counter_interval);
 
     Ok(())
 }
